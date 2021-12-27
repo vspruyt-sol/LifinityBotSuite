6fe15a4bcaf1694ec752ad8d6939d92a1771bb34
Valentijn Spruyt <Valentijnspruyt@gmail.com>
2021-12-27 14:58:37 +0100

    url error

--- src/helpers/discord-helper.ts
+++ src/helpers/discord-helper.ts
@@ -49,7 +49,7 @@ export default class DiscordHelper {
           "title": `${saleInfo.nftInfo.id} → SOLD (test)`,
           "url": `https://explorer.solana.com/tx/${saleInfo.txSignature}`,
           "thumbnail": {
-            "url": ` https://nft.lifinity.io/api/download/${saleInfo.nftInfo.id}`
+            "url": `https://nft.lifinity.io/api/download/${saleInfo.nftInfo.id}`
           },
           "timestamp": new Date(saleInfo.time * 1000).toISOString()
         }
